36ef5ffe144f0c7b6c9ca95ff9a9ca1fb10af3ba
duzicong <394043555@qq.com>
2017-04-12 17:23:54 +0800

    提交

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'DB_TYPE' => 'mysqli',
+    'DB_HOST' => 'localhost',   // 服务器地址
+    'DB_NAME' => 'followbee',// 数据库名
+    'DB_USER' => 'root',        // 账号
+    'DB_PWD' => '123456',             // 密码
+    'DB_PORT' => 3306,          // 端口
+    'DB_PREFIX' => 'hunuo_',          // 表前缀
+);
